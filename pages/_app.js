05626f2259eafe943ca5ae6b1e953d2a52348225
Bethany Berkowitz <bethanyb@unitworkers.com>
2023-02-13 15:57:18 -0500

    Reproduce _window.addEventListener is not a func

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Honeybadger, HoneybadgerErrorBoundary } from '@honeybadger-io/react'
+
+const honeybadger = Honeybadger.configure({
+  apiKey: 'hbp_LyehhF8SX7bxBtl1CBalPs1iUMo1952fv4Qx',
+  environment: 'production',
+  revision: 'hb-nextjs-react-test'
+})
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <HoneybadgerErrorBoundary honeybadger={honeybadger}>
+    <Component {...pageProps} />
+  </HoneybadgerErrorBoundary>
 }
